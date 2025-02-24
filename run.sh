86d63b1864a468c923c4efa705614bea80b22791
Iyán Méndez Veiga <me@iyanmv.com>
2025-02-24 22:29:23 +0100

    Install venv in a hidden directory
    Deploy job ignores hidden folders and directories. This will also avoid
    deleting the venv every there is a new commit.

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ ! -d venv ]; then
-    echo "venv does not exist. Creating virtual environment..."
-    python -m venv venv
-    venv/bin/pip install -r requirements.txt
+if [ ! -d .venv ]; then
+    echo ".venv does not exist. Creating virtual environment..."
+    python -m venv .venv
+    .venv/bin/pip install -r requirements.txt
 fi
 
 cd src
-../venv/bin/gunicorn -b 0.0.0.0:7000 -w 1 run:server
+../.venv/bin/gunicorn -b 0.0.0.0:7000 -w 1 run:server
