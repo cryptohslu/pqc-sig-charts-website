f69b46798f17ac6979cc85e289a4d779c6044e45
Iyán Méndez Veiga <me@iyanmv.com>
2025-06-06 13:05:25 +0200

    Improve script to generate datasets
    - Move the benchmarking logic to its own function
    - Use an adaptive strategy to benchmark algorithms instead of a fixed
      number of samples. This will ensure a more or less constant run time
      avoiding calling more than 10 times very slow algorithms (e.g.,
      all SLH-DSA algs or RSA keygen() alg)
    - By default, run at least 10 times each function. Or enough times so
      that the total time is greater than a second.
    - Store the parameters MIN_N_SAMPLES, MIN_SAMPLE_TIME_NS and SIZE_MESSAGE
      in the dataframe metadata
    - Add a debug argument to print additional information for each
      benchmark

--- generate_dataset.py
+++ generate_dataset.py
@@ -10,11 +10,39 @@ from Crypto.Hash import SHA512, SHAKE256
 from Crypto.PublicKey import ECC, RSA
 from Crypto.Signature import DSS, eddsa, pss
 
-N_SAMPLES = 10**3
+MIN_N_SAMPLES = 10
+MIN_SAMPLE_TIME_NS = 10**9
 SIZE_MESSAGE = 32
 
 
-def benchmark_traditional_sigs():
+def benchmark(func, *args, **kwargs):
+    """
+    Bencharmks func(*args, **kwargs) calling the function at least MIN_N_SAMPLES. It the function executes very fast,
+    more call will be done until MIN_SAMPLE_TIME_NS is reached.
+    It retunrs the tuple (average time, number of calls, total time). For convenience later, the average time is
+    returned in microseconds (μs), while the total time is returned as nanoseconds (ns) to avoid the precision loss
+    caused by the float type.
+    """
+    times = []
+    total_time_ns = 0
+    n = 0
+
+    while n < MIN_N_SAMPLES or total_time_ns < MIN_SAMPLE_TIME_NS:
+        start = time.perf_counter_ns()
+        func(*args, **kwargs)
+        end = time.perf_counter_ns()
+        times.append(end - start)
+        total_time_ns += end - start
+        n += 1
+
+    avg_time_us = sum(times) / n / 1e3
+    return avg_time_us, n, total_time_ns
+
+
+def benchmark_traditional_sigs(debug=False):
+    """
+    Here, a selection of classical digital signature algorithms are bencharmed.
+    """
     nist_sec_level = [1, 1, 1, 1, 3, 5, 1, 5]
     public_key_lengths = []
     secret_key_lenghts = []
@@ -44,22 +72,24 @@ def benchmark_traditional_sigs():
         "ed25519",
         "ed448",
     ]
-    n_sig_algs = len(sig_algs)
+    n_sig_algs = len(_rsa_bits) + len(_ecdsa) + len(_eddsa)
 
     print("Benchmarking traditional sig algs (RSA, ECC)")
 
+    i = 0
     for alg in _sig_algs:
         if alg == RSA:
             for bits in _rsa_bits:
-                print(f"RSASSA-PSS ({bits})", end="", flush=True)
+                print(f"RSASSA-PSS ({bits}) {i + 1}/{n_sig_algs}", end="", flush=True)
                 secret_key_lenghts.append(len(alg.generate(bits).export_key(format="DER")))
                 public_key_lengths.append(len(alg.generate(bits).public_key().export_key(format="DER")))
 
                 # Measure keygen
-                t = time.perf_counter_ns()
-                for i in range(N_SAMPLES):
-                    alg.generate(bits)
-                keygen_times.append((time.perf_counter_ns() - t) / N_SAMPLES / 1e3)
+                avg, n, total = benchmark(alg.generate, bits)
+                if debug:
+                    print(f"\nCalls to generate({bits}): {n}")
+                    print(f"Total time: {total}")
+                keygen_times.append(avg)
                 print(".", end="", flush=True)
 
                 # Measure sign
@@ -69,10 +99,11 @@ def benchmark_traditional_sigs():
                 h = SHA512.new(message)
                 signature_lenghts.append(len(signer.sign(h)))
 
-                t = time.perf_counter_ns()
-                for _ in range(N_SAMPLES):
-                    signer.sign(h)
-                sign_times.append((time.perf_counter_ns() - t) / N_SAMPLES / 1e3)
+                avg, n, total = benchmark(signer.sign, h)
+                if debug:
+                    print(f"\nCalls to sign(h): {n}")
+                    print(f"Total time: {total}")
+                sign_times.append(avg)
                 print(".", end="", flush=True)
 
                 # Measure verify
@@ -81,23 +112,27 @@ def benchmark_traditional_sigs():
                 signature = signer.sign(h)
                 pubkey = privkey.public_key()
                 verifier = pss.new(pubkey)
-                t = time.perf_counter_ns()
-                for _ in range(N_SAMPLES):
-                    verifier.verify(h, signature)
-                verify_times.append((time.perf_counter_ns() - t) / N_SAMPLES / 1e3)
+
+                avg, n, total = benchmark(verifier.verify, h, signature)
+                if debug:
+                    print(f"\nCalls to verify(h, signature): {n}")
+                    print(f"Total time: {total}")
+                verify_times.append(avg)
                 print(".✓")
+                i += 1
 
         elif alg == ECC:
             for curve in _ecdsa:
-                print(f"ECDSA ({curve})", end="", flush=True)
+                print(f"ECDSA ({curve}) {i + 1}/{n_sig_algs}", end="", flush=True)
                 secret_key_lenghts.append(len(alg.generate(curve=curve).export_key(format="DER")))
                 public_key_lengths.append(len(alg.generate(curve=curve).public_key().export_key(format="DER")))
 
                 # Measure keygen
-                t = time.perf_counter_ns()
-                for _ in range(N_SAMPLES):
-                    alg.generate(curve=curve)
-                keygen_times.append((time.perf_counter_ns() - t) / N_SAMPLES / 1e3)
+                avg, n, total = benchmark(alg.generate, curve=curve)
+                if debug:
+                    print(f"\nCalls to generate(curve={curve})): {n}")
+                    print(f"Total time: {total}")
+                keygen_times.append(avg)
                 print(".", end="", flush=True)
 
                 # Measure sign
@@ -107,10 +142,11 @@ def benchmark_traditional_sigs():
                 h = SHA512.new(message)
                 signature_lenghts.append(len(signer.sign(h)))
 
-                t = time.perf_counter_ns()
-                for _ in range(N_SAMPLES):
-                    signer.sign(h)
-                sign_times.append((time.perf_counter_ns() - t) / N_SAMPLES / 1e3)
+                avg, n, total = benchmark(signer.sign, h)
+                if debug:
+                    print(f"\nCalls to sign(h): {n}")
+                    print(f"Total time: {total}")
+                sign_times.append(avg)
                 print(".", end="", flush=True)
 
                 # Measure verify
@@ -119,23 +155,27 @@ def benchmark_traditional_sigs():
                 signature = signer.sign(h)
                 pubkey = privkey.public_key()
                 verifier = DSS.new(pubkey, mode="fips-186-3")
-                t = time.perf_counter_ns()
-                for i in range(N_SAMPLES):
-                    verifier.verify(h, signature)
-                verify_times.append((time.perf_counter_ns() - t) / N_SAMPLES / 1e3)
+
+                avg, n, total = benchmark(verifier.verify, h, signature)
+                if debug:
+                    print(f"\nCalls to verify(h, signature): {n}")
+                    print(f"Total time: {total}")
+                verify_times.append(avg)
                 print(".✓")
+                i += 1
 
             print("EdDSA", end="", flush=True)
             for curve in _eddsa:
-                print(f"EdDSA ({curve})", end="", flush=True)
+                print(f"EdDSA ({curve}) {i + 1}/{n_sig_algs}", end="", flush=True)
                 secret_key_lenghts.append(len(alg.generate(curve=curve).export_key(format="DER")))
                 public_key_lengths.append(len(alg.generate(curve=curve).public_key().export_key(format="DER")))
 
                 # Measure keygen
-                t = time.perf_counter_ns()
-                for _ in range(N_SAMPLES):
-                    alg.generate(curve=curve)
-                keygen_times.append((time.perf_counter_ns() - t) / N_SAMPLES / 1e3)
+                avg, n, total = benchmark(alg.generate, curve=curve)
+                if debug:
+                    print(f"\nCalls to generate(curve={curve}): {n}")
+                    print(f"Total time: {total}")
+                keygen_times.append(avg)
                 print(".", end="", flush=True)
 
                 # Measure sign
@@ -150,10 +190,11 @@ def benchmark_traditional_sigs():
                     raise RuntimeError
                 signature_lenghts.append(len(signer.sign(h)))
 
-                t = time.perf_counter_ns()
-                for _ in range(N_SAMPLES):
-                    signer.sign(h)
-                sign_times.append((time.perf_counter_ns() - t) / N_SAMPLES / 1e3)
+                avg, n, total = benchmark(signer.sign, h)
+                if debug:
+                    print(f"\nCalls to sign(h): {n}")
+                    print(f"Total time: {total}")
+                sign_times.append(avg)
                 print(".", end="", flush=True)
 
                 # Measure verify
@@ -167,14 +208,14 @@ def benchmark_traditional_sigs():
                 signature = signer.sign(h)
                 pubkey = privkey.public_key()
                 verifier = eddsa.new(pubkey, mode="rfc8032")
-                t = time.perf_counter_ns()
-                for _ in range(N_SAMPLES):
-                    # Temporary workaround (https://github.com/Legrandin/pycryptodome/issues/862)
-                    if curve == "ed448":
-                        h = SHAKE256.new(message)
-                    verifier.verify(h, signature)
-                verify_times.append((time.perf_counter_ns() - t) / N_SAMPLES / 1e3)
+
+                avg, n, total = benchmark(verifier.verify, h, signature)
+                if debug:
+                    print(f"\nCalls to verify(h, signature): {n}")
+                    print(f"Total time: {total}")
+                verify_times.append(avg)
                 print(".✓")
+                i += 1
 
     print("Saving dataframe", end="")
     df = pd.DataFrame(
@@ -189,16 +230,16 @@ def benchmark_traditional_sigs():
             "Verify (μs)": verify_times,
         }
     )
-    # df["Keygen (μs)"] = df["Keygen (μs)"].map(lambda x: f"{x:.1f}")
-    # df["Sign (μs)"] = df["Sign (μs)"].map(lambda x: f"{x:.1f}")
-    # df["Verify (μs)"] = df["Verify (μs)"].map(lambda x: f"{x:.1f}")
     df.set_index(["NIST", "Algorithm"], inplace=True)
     df.sort_index(inplace=True)
     print(" ✓")
     return df
 
 
-def benchmark_pqc_sigs():
+def benchmark_pqc_sigs(debug=False):
+    """
+    Here, available PQC digital signature algorithms in liboqs are bencharmed.
+    """
     nist_sec_level = []
     public_key_lengths = []
     secret_key_lenghts = []
@@ -222,27 +263,30 @@ def benchmark_pqc_sigs():
                 signature_lenghts.append(signer.length_signature)
 
                 # Measure keygen
-                t = time.perf_counter_ns()
-                for i in range(N_SAMPLES):
-                    signer.generate_keypair()
-                keygen_times.append((time.perf_counter_ns() - t) / N_SAMPLES / 1e3)
+                avg, n, total = benchmark(signer.generate_keypair)
+                if debug:
+                    print(f"\nCalls to generate_keypair(): {n}")
+                    print(f"Total time: {total}")
+                keygen_times.append(avg)
                 print(".", end="")
 
                 # Measure sign
                 pubkey = signer.generate_keypair()
                 message = secrets.token_bytes(SIZE_MESSAGE)
-                t = time.perf_counter_ns()
-                for i in range(N_SAMPLES):
-                    signer.sign(message)
-                sign_times.append((time.perf_counter_ns() - t) / N_SAMPLES / 1e3)
+                avg, n, total = benchmark(signer.sign, message)
+                if debug:
+                    print(f"\nCalls to sign(message): {n}")
+                    print(f"Total time: {total}")
+                sign_times.append(avg)
                 print(".", end="")
 
                 # Measure verify
                 signature = signer.sign(message)
-                t = time.perf_counter_ns()
-                for i in range(N_SAMPLES):
-                    verifier.verify(message, signature, pubkey)
-                verify_times.append((time.perf_counter_ns() - t) / N_SAMPLES / 1e3)
+                avg, n, total = benchmark(verifier.verify, message, signature, pubkey)
+                if debug:
+                    print(f"\nCalls to verify(message, signature, pubkey): {n}")
+                    print(f"Total time: {total}")
+                verify_times.append(avg)
                 print(".✓")
 
     print("\nSaving dataframe", end="")
@@ -258,9 +302,6 @@ def benchmark_pqc_sigs():
             "Verify (μs)": verify_times,
         }
     )
-    # df["Keygen (μs)"] = df["Keygen (μs)"].map(lambda x: f"{x:.1f}")
-    # df["Sign (μs)"] = df["Sign (μs)"].map(lambda x: f"{x:.1f}")
-    # df["Verify (μs)"] = df["Verify (μs)"].map(lambda x: f"{x:.1f}")
     df.set_index(["NIST", "Algorithm"], inplace=True)
     df.sort_index(inplace=True)
     print(" ✓")
@@ -268,16 +309,22 @@ def benchmark_pqc_sigs():
 
 
 def main():
+    debug = False
     t0 = time.perf_counter()
-    df_traditional = benchmark_traditional_sigs()
+    print(f"MIN_N_SAMPLES = {MIN_N_SAMPLES}")
+    print(f"MIN_SAMPLE_TIME_NS = {MIN_SAMPLE_TIME_NS}")
+    print(f"SIZE_MESSAGE = {SIZE_MESSAGE}\n")
+    df_traditional = benchmark_traditional_sigs(debug)
     print()
-    df_pqc = benchmark_pqc_sigs()
+    df_pqc = benchmark_pqc_sigs(debug)
     df = pd.concat([df_traditional, df_pqc]).sort_index()
     t1 = time.perf_counter() - t0
     df.attrs = {
         "timestampt": datetime.isoformat(datetime.now()),
         "duration": t1,
-        "n_samples": N_SAMPLES,
+        "MIN_N_SAMPLES": MIN_N_SAMPLES,
+        "MIN_SAMPLE_TIME_NS": MIN_SAMPLE_TIME_NS,
+        "SIZE_MESSAGE": SIZE_MESSAGE,
         "liboqs_version": oqs.oqs_python_version(),
         "pycryptodome_version": Crypto.__version__,
     }
