5607aeafa5acdfa03ea86911ba251ab14621055a
Iyán Méndez Veiga <me@iyanmv.com>
2025-09-03 17:23:34 +0200

    Set HTTP security headers

--- src/run.py
+++ src/run.py
@@ -1,8 +1,7 @@
-from pathlib import Path
-
 import dash
 from dash import Dash
-from flask import Flask
+from flask import Flask, Response
+from secure import Secure
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from components.appshell import create_appshell
@@ -10,6 +9,14 @@ from components.appshell import create_appshell
 server = Flask(__name__)
 server.wsgi_app = ProxyFix(server.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 server.config["APPLICATION_ROOT"] = "/sig-charts/"
+secure_headers = Secure.with_default_headers()
+
+
+@server.after_request
+def set_secure_headers(response: Response):
+    secure_headers.set_headers(response)
+    return response
+
 
 app = Dash(
     __name__,
